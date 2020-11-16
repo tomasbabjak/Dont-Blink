c5e0838279aea9720736dfd1fa42078d494e6249
tomasbabjak <32101724+tomasbabjak@users.noreply.github.com>
2020-11-16 13:43:16 +0100

    simple door

---
+++ Assets/scripts/DoorControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public bool doorKey;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (doorKey)
+            {
+                var newRot = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+                transform.rotation = newRot;
+            }
+        }
+    }
+}
---
+++ Assets/scripts/KeyControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+{
+    public DoorControl doorScript;
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorScript.doorKey = true;
+            door.GetComponent<BoxCollider>().isTrigger = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
